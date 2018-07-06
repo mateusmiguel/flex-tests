bfe95e50e1b7751d7c321af48008fb4252038202
Mateus Miguel <webmateusmiguel@gmail.com>
2018-07-06 14:05:27 +0000

    Refactory

--- js/main.js
+++ js/main.js
@@ -1,40 +1,23 @@
 var cards = function () {
-
     function cardCalc(el) {
-        return 'rodei';
+
+        var lenght = []
+
+        $(el).each(function () {
+            $(this).attr('style', 'height: ')
+            lenght.push($(this).height())
+            lenght.sort()
+        })
+
+        var height = lenght.pop()
+        
+        $(el).each(function () {
+            $(this).attr('style', 'height: ' + height + 'px')
+        })
     }
 
-    console.log(cardCalc());
-
-    var titleLenght = []
-
-    $('.card-group .card .card-title').each(function () {
-        $(this).attr('style', 'height: ');
-        titleLenght.push($(this).height())
-        titleLenght.sort()
-    })
-
-    var titleHeight = titleLenght.pop()
-
-    $('.card-group .card .card-title').each(function () {
-        $(this).attr('style', 'height: ' + titleHeight + 'px')
-    })
-
-    subtitleLenght = []
-
-    $('.card-group .card .card-sub').each(function () {
-        $(this).attr('style', 'height: ');
-        subtitleLenght.push($(this).outerHeight(true))
-        subtitleLenght.sort()
-    })
-
-    var subtitleHeight = subtitleLenght.pop()
-
-    $('.card-group .card .card-sub').each(function () {
-        $(this).attr('style', 'height: ' + subtitleHeight + 'px')
-    })
-
-    console.log('rodou')
+    cardCalc('.card-group .card .card-title');
+    cardCalc('.card-group .card .card-sub');
 }
 
 window.onload = cards
